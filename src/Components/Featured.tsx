779857b6aeaee0a3710fe32641ec2a2a6fc07c2d
Muhammad Imran Khan <Muhammad-Imran-Khan>
2023-08-05 10:38:50 -0400

    starting featured design

--- src/Components/Featured.tsx
+++ src/Components/Featured.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Featured = () => {
   return (
-    <div>Featured</div>
-  )
-}
+    <div className="w-screen overflow-x-scroll text-red-400">
+      {/* wrapper */}
+      <div className="w-max flex">
+        {/* single item */}
 
-export default Featured
+        <div className="w-screen h-[60vh] flex flex-col items-center justify-around p-4">
+          {/* image container */}
+          <div className="relative flex-1 w-full ">
+            <Image src="/temporary/p1.png" alt="" fill className="object-contain" />
+          </div>
+          <div className="flex flex-col  gap-4 flex-1">
+            {/* text container */}
+            <h1 className="text-xl font-bold uppercase">Title</h1>
+            <p className="">Desc</p>
+            <span className="text-xl font-bold">$ 141</span>
+            <button className="bg-red-400 text-white p-2 rounded-md">add to cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
--- src/Components/Slider.tsx
+++ src/Components/Slider.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+"use client";
+import Image from "next/image";
+import React, { useState, useEffect } from "react";
+
+const data = [
+  {
+    id: 1,
+    title: "always fresh & always crispy & always hot",
+    image: "/slide1.png",
+  },
+  {
+    id: 2,
+    title: "We deliver your order wherever you are in NJ",
+    image: "/slid2.png",
+  },
+  {
+    id: 3,
+    title: "The best pizza to share with your family",
+    image: "/slide3.jpg",
+  },
+];
 
 const Slider = () => {
-  return (
-    <div>Slider</div>
-  )
-}
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-export default Slider
+  useEffect(() => {
+    const interval = setInterval(() =>
+      setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1)),3000
+    );
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="flex flex-col h-[calc(100vh-4rem)] md:h-calc[100vh-6rem)] lg:flex-row bg-fuchsia-50">
+      {/* Text container */}
+      <div className="flex-1 h-1/2 flex flex-col items-center justify-center gap-8 text-red-400 font-bold lg:h-full lg:w-1/2">
+        <h1 className="font-5xl text-center uppercase p-4  md:text-4xl lg:text-5xl ">
+          {data[currentSlide].title}
+        </h1>
+        <button className="bg-red-400  text-white py-4 px-8">Order now</button>
+      </div>
+      {/* image container */}
+      <div className="flex-1 w-full h-1/2 relative lg:h-full lg:w-1/2">
+        <Image
+          src={data[currentSlide].image}
+          alt=""
+          fill
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
