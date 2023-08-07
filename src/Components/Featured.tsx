327867931cc85f508dbbf769fee4ce229c4bef78
Muhammad Imran Khan <Muhammad-Imran-Khan>
2023-08-07 04:21:41 -0400

    working on offer

--- src/Components/Featured.tsx
+++ src/Components/Featured.tsx
@@ -1,3 +1,4 @@
+import { featuredProducts } from "@/data";
 import Image from "next/image";
 import React from "react";
 
@@ -8,19 +9,30 @@ const Featured = () => {
       <div className="w-max flex">
         {/* single item */}
 
-        <div className="w-screen h-[60vh] flex flex-col items-center justify-around p-4">
-          {/* image container */}
-          <div className="relative flex-1 w-full ">
-            <Image src="/temporary/p1.png" alt="" fill className="object-contain" />
+        {featuredProducts.map((item) => (
+          <div
+            key={item.id}
+            className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 text-center hover:bg-fuchsia-50 transition-all duration-300
+            md:w-[50vw] xl:w-[33vw] xl:h-[90vh]
+            "
+          >
+            {/* image container */}
+            {item.img && (
+              <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500">
+                <Image src={item.img} alt="" fill className="object-contain" />
+              </div>
+            )}
+            <div className="flex flex-col  gap-4 flex-1 items-center justify-center ">
+              {/* text container */}
+              <h1 className="text-xl font-bold uppercase xl:text-2xl 2x:text-3xl">{item.title}</h1>
+              <p className="p-4 2xl:p-8">{item.desc}</p>
+              <span className="text-xl font-bold">$ {item.price}</span>
+              <button className="bg-red-400 text-white p-2 rounded-md uppercase">
+                add to cart
+              </button>
+            </div>
           </div>
-          <div className="flex flex-col  gap-4 flex-1">
-            {/* text container */}
-            <h1 className="text-xl font-bold uppercase">Title</h1>
-            <p className="">Desc</p>
-            <span className="text-xl font-bold">$ 141</span>
-            <button className="bg-red-400 text-white p-2 rounded-md">add to cart</button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
